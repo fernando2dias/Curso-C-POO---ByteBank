9c5f21578e58b6633cbc964d7a54ac1b8c650c6b
Fernando Dias Motta <180016@facens.br>
2020-07-19 13:15:56 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 33453;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
