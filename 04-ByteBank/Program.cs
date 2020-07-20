9340a382e7d3efdf1313195ec8846f0c8fa743b4
Fernando Dias Motta <180016@facens.br>
2020-07-20 15:10:49 -0300

    arrumando

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaFernando = new ContaCorrente();
+            contaFernando.titular = "Fernando";
+            contaFernando.Sacar(45.20);
+
+            Console.WriteLine(contaFernando.saldo);
+
+            contaFernando.Depositar(500);
+
+            Console.WriteLine(contaFernando.saldo);
+
+            ContaCorrente contaJuba = new ContaCorrente();
+            contaJuba.titular = "Jubela";
+
+            contaFernando.Transferir(300, contaJuba);
+
+            Console.WriteLine("meu saldo: " + contaFernando.saldo);
+            Console.WriteLine("saldo juba" + contaJuba.saldo );
+
+
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
